c82139c73567dead4579c86efbf8b942f99941ee
Erkan_filiz <erkanfiliz1715@gmail.com>
2023-10-15 02:44:22 +0200

    react-useeffect-axious-app

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import TutorialList from "../components /TutorialList";
+import AddTutorial from "../components /AddTutorial";
+
+
+const Home = () => {
+  return (
+    <>
+      <AddTutorial/>
+      <TutorialList/>
+    </>
+  );
+};
+
+export default Home;
