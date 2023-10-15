1177674486029c331d7908fd0d57346d2b7953f8
Erkan_filiz <erkanfiliz1715@gmail.com>
2023-10-16 01:12:28 +0200

    axios-react-app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import TutorialList from "../components /TutorialList";
 import AddTutorial from "../components /AddTutorial";
-
-
+import axios from "axios"
+import {useEffect, useState} from "react"
 const Home = () => {
-  return (
+   const [tutorials, setTutorials]=useState([])
+  const BASE_URL="https://tutorial-api.fullstack.clarusway.com/tutorials/"
+  const getTutorials=async()=>
+  {
+    const data=await axios(BASE_URL)
+    setTutorials(data.data)
+}
+console.log(tutorials)
+
+useEffect(()=>{
+  getTutorials()
+},[])
+return (
     <>
       <AddTutorial/>
       <TutorialList/>
